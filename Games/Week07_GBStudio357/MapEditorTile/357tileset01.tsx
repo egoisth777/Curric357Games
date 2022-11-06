f201818bdea26c16d30f96fd298d04d158f2fd03
ezel <egoistheresy@outlook.com>
2022-11-06 15:54:14 -0600

    pushed games

---
+++ Games/Week07_GBStudio357/MapEditorTile/357tileset01.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="357tileset01" tilewidth="8" tileheight="8" tilecount="160" columns="16">
+ <transformations hflip="1" vflip="1" rotate="1" preferuntransformed="0"/>
+ <image source="tileset01.png" width="128" height="80"/>
+ <wangsets>
+  <wangset name="Mockup" type="mixed" tile="-1">
+   <wangcolor name="Dirt" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Air" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="1" wangid="1,2,2,2,2,2,2,2"/>
+   <wangtile tileid="2" wangid="1,2,1,2,2,2,2,2"/>
+   <wangtile tileid="3" wangid="1,1,1,2,2,2,2,2"/>
+   <wangtile tileid="4" wangid="1,2,2,2,1,2,2,2"/>
+   <wangtile tileid="5" wangid="1,2,1,2,1,2,2,2"/>
+   <wangtile tileid="6" wangid="1,1,1,2,1,2,2,2"/>
+   <wangtile tileid="7" wangid="1,2,1,1,1,2,2,2"/>
+   <wangtile tileid="8" wangid="1,1,1,1,1,2,2,2"/>
+   <wangtile tileid="9" wangid="1,2,1,2,1,2,1,2"/>
+   <wangtile tileid="10" wangid="1,1,1,2,1,2,1,2"/>
+   <wangtile tileid="11" wangid="1,1,1,1,1,2,1,2"/>
+   <wangtile tileid="12" wangid="1,1,1,2,1,1,1,2"/>
+   <wangtile tileid="13" wangid="1,1,1,1,1,1,1,2"/>
+   <wangtile tileid="14" wangid="1,1,1,1,1,1,1,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ Games/Week07_GBStudio357/MapEditorTile/forestSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="forestSet" tilewidth="16" tileheight="16" tilecount="99" columns="11">
+ <image source="forest_tileset.png" width="176" height="144"/>
+</tileset>
